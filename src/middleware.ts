b615c24eb8d4cb7bb540d5ac4f6a750ede8d50de
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-10-28 00:56:02 +0300

    vercel fix matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
     // Match only internationalized pathnames
-    matcher: ['/', '/(tr|en)/:path*']
+    matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
