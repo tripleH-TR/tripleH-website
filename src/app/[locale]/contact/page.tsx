4ccefec1c554e81a2d53eb420ffccb8c89074e1a
kasimpasaoglu <kasimpasaoglu@windowslive.com>
2024-10-29 22:12:10 +0300

    fix

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,6 +1,5 @@
 import Cards from "@/app/components/contact/Cards"
 import Form from "@/app/components/contact/Form"
-import Map from "@/app/components/contact/Map"
 import Header from "@/app/components/Header"
 import { useTranslations } from "next-intl"
 
